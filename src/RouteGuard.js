077e62989611053c075595da49daef80f406d5da
jefferdeveloper <jefferdeveloper@gmail.com>
2020-12-02 09:35:53 +0800

    route guard

--- src/RouteGuard.js
+++ src/RouteGuard.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Redirect } from "react-router-dom";
 
-export default function GuardRoute({
+export default function RouteGuard({
   component: Component,
   children,
   guards,
